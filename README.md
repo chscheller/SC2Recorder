1672a7104378c2159f98ed356af976e17551ea5b
Christian Scheller <ch.scheller@hotmail.com>
2018-03-20 15:48:24 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # SC2Recorder
+
+## Installation
+
+### Starcraft II
+
+#### Windows & Mac
+
+Download and install from the [official website](https://starcraft2.com/) (Starcraft II is free to play by now).
+
+Open [DemoReplayFile.SC2Replay](DemoReplay_3-16-1.SC2Replay) (directly with the Starcraft II client) to ensure that Starcraft II has downloaded the required version.
+
+#### Linux
+
+Download Starcraft II Linux package **Version 3.16.1** from the Blizzard sc2client [GitHub repository](https://github.com/Blizzard/s2client-proto#downloads).
+
+Be sure to copy the files to `~/StarCraftII/`.
+
+### Maps
+
+Download the [mini games](https://github.com/deepmind/pysc2/releases/download/v1.2/mini_games.zip) and extract them to your `StarcraftII/Maps/` directory.
+
+### PySC2
+
+Install Deepminds [PySC2](https://github.com/deepmind/pysc2) package using pip: 
+
+```
+pip install pysc2==1.2
+```
+
+## Run Recorder
+
+Record replays using the following command:
+
+```
+python record.py
+```
+
+By default replays will be stored under `./replays/`.
---
+++ record.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import ntpath
+import pathlib
+import platform
+import shutil
+import sys
+import time
+import os
+
+from absl.flags import FLAGS
+from pysc2 import maps
+from pysc2 import run_configs
+from pysc2.env import sc2_env
+from s2clientprotocol import sc2api_pb2 as sc_pb
+
+
+def copy_replay_to_project_path(
+        replay_file_path: str,
+        score: any,
+        project_replay_path: str="./replays"
+) -> None:
+    pathlib.Path(project_replay_path).mkdir(parents=True, exist_ok=True)
+    replay_name = ntpath.basename(replay_file_path)
+    project_replay_file_path = os.path.join(project_replay_path, replay_name)
+    shutil.copyfile(replay_file_path, project_replay_file_path)
+    replay_score_file_path = project_replay_file_path.replace("SC2Replay", "txt")
+    with open(replay_score_file_path, "w") as f:
+        f.write("{}\n".format(score))
+    print("Replay saved to: {}".format(project_replay_file_path))
+
+
+def record(
+        map_name: str= "CollectMineralsAndGas",
+        sc2_version: str="3.16.1",
+        realtime: bool=True,  # False,
+        full_screen: bool=False,
+        # fps: float=22.4,
+        fps: float=60,
+        step_mul: int=1,
+        user_race: int="R",
+        bot_race: int="R",
+        difficulty: int="1",
+        disable_fog: bool=False
+) -> None:
+    if platform.system() == "Linux" and (realtime or full_screen):
+        sys.exit("realtime and full_screen only make sense on Windows/MacOS.")
+
+    run_config = run_configs.get()
+
+    map_inst = maps.get(map_name)
+
+    create = sc_pb.RequestCreateGame(
+        realtime=realtime,
+        disable_fog=disable_fog,
+        local_map=sc_pb.LocalMap(
+            map_path=map_inst.path,
+            map_data=map_inst.data(run_config)
+        )
+    )
+    create.player_setup.add(
+        type=sc_pb.Participant
+    )
+    create.player_setup.add(
+        type=sc_pb.Computer,
+        race=sc2_env.races[bot_race],
+        difficulty=sc2_env.difficulties[difficulty]
+    )
+    join = sc_pb.RequestJoinGame(
+        race=sc2_env.races[user_race],
+        options=sc_pb.InterfaceOptions()
+    )
+
+    with run_config.start(
+            game_version=sc2_version,
+            full_screen=full_screen
+    ) as controller:
+        controller.create_game(create)
+        controller.join_game(join)
+        obs = controller.observe()
+        result = obs.player_result
+        try:
+            while not result:
+                frame_start_time = time.time()
+                if not realtime:
+                    controller.step(step_mul)
+                obs = controller.observe()
+                result = obs.player_result
+                time.sleep(max(0, frame_start_time + 1 / fps - time.time()))
+        except KeyboardInterrupt:
+            pass
+        print("Score: ", obs.observation.score.score)
+        print("Result: ", obs.player_result)
+        replay_file_path = run_config.save_replay(
+            replay_data=controller.save_replay(),
+            replay_dir="local",
+            map_name=map_name
+        )
+        copy_replay_to_project_path(
+            replay_file_path=replay_file_path,
+            score=obs.observation.score.score
+        )
+
+
+if __name__ == "__main__":
+    FLAGS(sys.argv)
+    record()
